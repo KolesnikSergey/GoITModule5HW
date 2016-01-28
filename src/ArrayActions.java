bc73d3b1e30926bd27b35654bcc756a2d6ee0671
Sergey Kolesnik <s.kolesnik@bpcbt.com>
2016-01-28 21:21:54 +0200

    some changes

--- src/ArrayActions.java
+++ src/ArrayActions.java
@@ -3,6 +3,7 @@
  */
 public class ArrayActions {
     private int[] numbers;
+    private String sortingName = "Gnome Sorting";
 
     public ArrayActions(int[] numbers) {
         this.numbers = numbers;
@@ -12,31 +13,52 @@ public class ArrayActions {
         this.numbers = numbers;
     }
 
+    public String getSortingName() {
+        return sortingName;
+    }
+
     /**
      * Find minimal value and return it
      * @return
+     * @param inMinMax - is string where "MIN" when find minimal value,
+     *                   "MAX" - when maximal.
      */
-    public int findMinimal () {
+    public int findMinMax (String inMinMax ) {
         int counter = 0;
-        int minimal = this.numbers[0];
+        int minMax = this.numbers[0];
 
         while (counter < this.numbers.length) {
-            if (minimal > this.numbers[counter])
-                minimal = this.numbers[counter];
+            switch (inMinMax) {
+                case "MAX" :
+                    if (minMax < this.numbers[counter])
+                        minMax = this.numbers[counter];
+                    break;
+                case "MIN" :
+                    if (minMax > this.numbers[counter])
+                        minMax = this.numbers[counter];
+                    break;
+                default:
+                    System.out.println("Unknown find method ["+inMinMax+"] you should use MIN or MAX");
+                    return 0;
+            }
+
             counter++;
         }
 
-        return minimal;
+        System.out.println( inMinMax+" is "+minMax+".");
+        return minMax;
     }
 
     /**
      * This method sort array
+     * with Gnome sorting method.
      */
-    public int[] someSorting () {
+    public int[] gnomeSorting () {
         printArray(true);
         int counter = 1;
         while(counter < this.numbers.length) {
-            if(counter==0 || this.numbers[counter - 1] <= this.numbers[counter])
+            if(counter==0 ||
+               this.numbers[counter - 1] <= this.numbers[counter])
                 counter++;
             else {
                 int temp = this.numbers[counter];
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,57 @@
+import java.util.Scanner;
+
 /**
  * Created by kolesnik_s on 09.01.16.
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int[] num;
+        ArrayActions arrAct;
+        int counter = 0;
+
+        /**
+         * Enter array length
+         */
+        System.out.print("Enter array length: ");
+        num = new int[keyboard.nextInt()];
+
+        /**
+         * Entering data for array
+         */
+        System.out.print("Enter numbers for array:");
+        while (num.length > counter) {
+            num[counter] = keyboard.nextInt();
+            counter++;
+            System.out.print((counter == num.length) ? "last.\n" : "next:");
+        }
+
+        /**
+         * Create object arrAct
+         */
+        arrAct = new ArrayActions(num);
+
         /**
          * Start first task from 5 module
          */
-        int[] num = {3,5,7,8,9,12,2};
-        ArrayActions arrAct;
-        arrAct = new ArrayActions(num);
-        System.out.println("Minimal is "+arrAct.findMinimal());
+        arrAct.findMinMax("MIN");
         /**
          * End first task
          */
 
         /**
+         * Find MAX value
+         */
+        arrAct.findMinMax("MAX");
+        /**
+         * End find MAX
+         */
+
+        /**
          * Start second task from 5 module
          */
-         arrAct.someSorting();
+        System.out.println("Use "+arrAct.getSortingName());
+        arrAct.gnomeSorting();
         /**
          * End second task
          */
